5473407e904a26cf7708d1cd16ae6984aff7879f
babinec <mario.babinec@ui42.com>
2024-06-28 08:09:03 +0200

    feat: add last instagram fetch info text to login form

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 use AllowDynamicProperties;
 use App\Controller\BaseController;
 use Exception;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -20,15 +22,22 @@ use Twig\Environment;
 
     /**
      * @Route("/login", name="app_login")
+     * @throws InvalidArgumentException
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+        $cache = new FilesystemAdapter();
+        $key = 'last_request_time';
+        $lastRequestTime = $cache->getItem($key)->get();
+        date_default_timezone_set('Europe/Bratislava');
+        $localDateTime = date('d. m. Y H:i:s', $lastRequestTime);
 
         return $this->render('Security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'last_time_instagram_fetch' => $localDateTime,
         ]);
     }
 
