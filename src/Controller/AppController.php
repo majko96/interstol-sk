bf23cbe6d31194d297627e2825a351b59011db59
Mario Babinec <mr.babinec@gmail.com>
2025-07-29 13:33:40 +0200

    fix: fix gallery names

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -70,8 +70,8 @@ class AppController extends BaseController
             'kitchen_bohus' => 'Kuchyňa - Považská Bystrica',
             'kitchen_dvorscik' => 'Kuchyňa - Považská Bystrica',
             'kitchen_klimoszek' => 'Kuchyňa - Považská Bystrica',
-            'kitchen_oskrobana' => 'Kuchyňa Bodiná',
-            'kitchen_teplicka' => 'Kuchyňa Teplička nad Váhom',
+            'kitchen_oskrobana' => 'Kuchyňa - Bodiná',
+            'kitchen_teplicka' => 'Kuchyňa - Teplička nad Váhom',
         ];
 
         $galleries = [];
